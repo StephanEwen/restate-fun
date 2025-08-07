d2bf069db4f4d448ce02567bae5ad44ccab3704f
igalshilman <igal@restate.dev>
2025-08-07 17:47:31 +0200

    Add an warning if OPENAI_API_KEY is missing

--- mini-coding-agent-template/src/app.ts
+++ mini-coding-agent-template/src/app.ts
@@ -1,7 +1,18 @@
+import { env } from "node:process";
+
 import { serve  } from "@restatedev/restate-sdk";
 import { agent } from "./agent";
 import { agentExecutor } from "./agent_executor";
 import { sandbox } from "./sandbox";
+
+
+if (env.OPENAI_API_KEY === undefined) {
+  console.warn(`
+    ----------------------------------------------------------
+    | WARNING: OPENAI_API_KEY is not set in the environment. |
+    ----------------------------------------------------------
+    `);
+}
 
 serve({
   services: [agent, agentExecutor, sandbox],
